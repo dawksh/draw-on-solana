f1080a9e4f7e539754f06a23c6b04db609e80d5d
Daksh <akshd@outlook.com>
2022-09-25 19:53:14 +0530

    feat: add errors in test

--- programs/draw/src/lib.rs
+++ programs/draw/src/lib.rs
@@ -6,14 +6,35 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod draw {
     use super::*;
 
+    const MIN_POS: u8 = 0;
+    const MAX_POS: u8 = 99;
+    const MIN_COL: u8 = 0;
+    const MAX_COL: u8 = 255;
+
     pub fn create_pixel(ctx: Context<CreatePixel>, pos_x:u8, pos_y: u8, init_col_r: u8, init_col_g: u8, init_col_b: u8) -> Result<()> {
+        
+        if pos_x < MIN_POS || pos_x > MAX_POS {
+            return Err(error!(ErrorCode::InvalidXCoordinate));
+        }
+        if pos_y < MIN_POS || pos_y > MAX_POS {
+            return Err(error!(ErrorCode::InvalidYCoordinate));
+        }
+        if init_col_g < MIN_COL || init_col_g > MAX_COL {
+            return Err(error!(ErrorCode::InvalidGColor));
+        }
+        if init_col_r < MIN_COL || init_col_r > MAX_COL {
+            return Err(error!(ErrorCode::InvalidRColor));
+        }
+        if init_col_b < MIN_COL || init_col_b > MAX_COL {
+            return Err(error!(ErrorCode::InvalidBColor));
+        }
+        
         let pixel = &mut ctx.accounts.pixel;
         pixel.pos_x = pos_x;
         pixel.pos_y = pos_y;
         pixel.col_r = init_col_r;
         pixel.col_g = init_col_g;
         pixel.col_b = init_col_b;
- 
         Ok(())
     }
 }
@@ -37,6 +58,20 @@ pub struct Pixel {
     pub col_r: u8,
     pub col_g: u8,
     pub col_b: u8,
+}
+
+#[error_code]
+pub enum ErrorCode{
+     #[msg("The given X co-ordinate is not between 0-99")]
+    InvalidXCoordinate,
+    #[msg("The given Y co-ordinate is not between 0-99")]
+    InvalidYCoordinate,
+    #[msg("The given R color is not between 0-255")]
+    InvalidRColor,
+    #[msg("The given G color is not between 0-255")]
+    InvalidGColor,
+    #[msg("The given B color is not between 0-255")]
+    InvalidBColor,
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8;
--- tests/draw.ts
+++ tests/draw.ts
@@ -1,16 +1,32 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, AnchorError } from "@project-serum/anchor";
+import { assert } from "chai";
 import { Draw } from "../target/types/draw";
 
 describe("draw", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const anchorProvider = anchor.AnchorProvider.env();
+  anchor.setProvider(anchorProvider);
 
   const program = anchor.workspace.Draw as Program<Draw>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  it("can create a new pixel" , async () => {
+
+    const keypair = anchor.web3.Keypair.generate()
+
+    await program.methods.createPixel(0, 200, 0, 0, 255).accounts({
+      pixel: keypair.publicKey,
+      user: anchorProvider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).signers([keypair]).rpc().then(() => Promise.reject(new Error('Expected to error!')), (e: AnchorError) => {
+      assert.ok(e.errorLogs.some(log => log.includes('InvalidYCoordinate') && log.includes('The given Y co-ordinate is not between 0-99.')))
+    })
+
+    const storedPixel = await program.account.pixel.fetch(keypair.publicKey)
+    assert.equal(storedPixel.posX, 10)
+    assert.equal(storedPixel.posY, 10)
+    assert.equal(storedPixel.colR, 0)
+    assert.equal(storedPixel.colG, 0)
+    assert.equal(storedPixel.colB, 255)
+  })
 });
